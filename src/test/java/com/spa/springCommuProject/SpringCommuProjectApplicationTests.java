74b5d17b155e3eb26b8c29c30cd31ee68d5135c6
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-06-29 23:33:59 +0900

    feat: "yml profile로 dev, prod 구분"

--- src/test/java/com/spa/springCommuProject/SpringCommuProjectApplicationTests.java
+++ src/test/java/com/spa/springCommuProject/SpringCommuProjectApplicationTests.java
@@ -1,13 +1,10 @@
 package com.spa.springCommuProject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCommuProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
