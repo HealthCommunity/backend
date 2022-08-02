eb6cd3f0ed0b892005a3ffe718ad9dd35b959fe6
changjun6518 <aowert6518@gmail.com>
2022-08-02 21:03:09 +0900

    update: 로그인 성공 후 헤더에 session 값 넘기기

--- src/main/java/com/spa/springCommuProject/config/CustumLoginSuccessHandler.java
+++ src/main/java/com/spa/springCommuProject/config/CustumLoginSuccessHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,8 @@ public class CustumLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
         throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        response.addHeader("Session", session.getId());
         response.sendRedirect("/");
     }
 }
