1ec77832fff77fcb1b187623907d190e16611f50
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-08-04 17:02:54 +0900

    test: CORS테스트

--- src/main/java/com/spa/springCommuProject/config/CustumLoginSuccessHandler.java
+++ src/main/java/com/spa/springCommuProject/config/CustumLoginSuccessHandler.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Component
@@ -16,9 +15,5 @@ public class CustumLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
         throws IOException, ServletException {
-        HttpSession session = request.getSession();
-        response.addHeader("Session", session.getId());
-        session.setAttribute("JSESSIONID", session.getId());
-        response.sendRedirect("/");
     }
 }
