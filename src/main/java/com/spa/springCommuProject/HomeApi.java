3528f5da8bc34012bb7afca4fdc5363607b2015f
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-08-16 21:53:09 +0900

    fix : 검색기능 수정

--- src/main/java/com/spa/springCommuProject/HomeApi.java
+++ src/main/java/com/spa/springCommuProject/HomeApi.java
@@ -9,10 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +27,7 @@ public class HomeApi {
         return ResponseEntity.ok(CommonResponse.from(postService.mainPagePosts()));
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     @ApiOperation(value = "검색")
     public ResponseEntity<CommonResponse<List<PostViewDTO>>> search(@RequestBody SearchRequest searchRequest){
 
