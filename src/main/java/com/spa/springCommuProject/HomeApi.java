4e7e50e15f0b7f046f4a0ed4fbbd3f41f8a3579c
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-08-16 21:44:18 +0900

    fix : 검색기능 수정

--- src/main/java/com/spa/springCommuProject/HomeApi.java
+++ src/main/java/com/spa/springCommuProject/HomeApi.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,7 +32,10 @@ public class HomeApi {
 
     @GetMapping("/search")
     @ApiOperation(value = "검색")
-    public ResponseEntity<CommonResponse<List<PostViewDTO>>> search(SearchRequest searchRequest){
+    public ResponseEntity<CommonResponse<List<PostViewDTO>>> search(@RequestBody SearchRequest searchRequest){
+
+        //작성자, 내용, 제목, 통합검색 (제목 + 내용)
+
         if(searchRequest.getSelect().equals("title")){
             return ResponseEntity.ok(CommonResponse.from(postService.searchTitle(searchRequest.getKeyword())));
         }else {
--- src/main/java/com/spa/springCommuProject/config/SecurityConfig.java
+++ src/main/java/com/spa/springCommuProject/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and()
                 .csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers("/search").permitAll()
                 .antMatchers("/api/user/login").permitAll()
                 .antMatchers("/api/user/join").permitAll()
                 .antMatchers("/api/exercisepost").permitAll()
