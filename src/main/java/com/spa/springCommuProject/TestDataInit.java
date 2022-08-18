b9e6d2be5a8263003848dd41b6ac8d871ecb5077
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-08-18 20:42:03 +0900

    feat : test데이터 삭제

--- src/main/java/com/spa/springCommuProject/TestDataInit.java
+++ src/main/java/com/spa/springCommuProject/TestDataInit.java
@@ -2,7 +2,6 @@ package com.spa.springCommuProject;
 
 
 import com.spa.springCommuProject.posts.repository.PostRepository;
-import com.spa.springCommuProject.user.domain.User;
 import com.spa.springCommuProject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,18 +22,18 @@ public class TestDataInit {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @PostConstruct
     public void createDummyData() {
-        String password = bCryptPasswordEncoder.encode("1234");
-        String password2 = bCryptPasswordEncoder.encode("1234");
-
-        User june = new User("june", "june", password);
-        User hoon = new User("hoon", "hoon", password);
-        User tail = new User("tail", "tail", password2);
-        User song = new User("song", "song", password2);
-
-        userRepository.save(june);
-        userRepository.save(hoon);
-        userRepository.save(tail);
-        userRepository.save(song);
+//        String password = bCryptPasswordEncoder.encode("1234");
+//        String password2 = bCryptPasswordEncoder.encode("1234");
+//
+//        User june = new User("june", "june", password);
+//        User hoon = new User("hoon", "hoon", password);
+//        User tail = new User("tail", "tail", password2);
+//        User song = new User("song", "song", password2);
+//
+//        userRepository.save(june);
+//        userRepository.save(hoon);
+//        userRepository.save(tail);
+//        userRepository.save(song);
 //        ArrayList<Post> posts = new ArrayList<>();
 //        for (int i = 0; i < 100; i++) {
 //            posts.add(new Post(june, "title" + i, "content" + i, PostCategory.EXERCISEPOST));
