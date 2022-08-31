9b494c51aff172961725f538d46bbb802a7dd2a9
changjun6518 <aowert6518@gmail.com>
2022-08-31 12:07:25 +0900

    feat: 댓글 요청 권한 허용

--- src/main/java/com/spa/springCommuProject/config/SecurityConfig.java
+++ src/main/java/com/spa/springCommuProject/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/freepost/{postId}").permitAll()
                 .antMatchers("/api/threepowerpost").permitAll()
                 .antMatchers("/api/threepowerpost/{postId}").permitAll()
-//                .antMatchers("/oauth2/authorization/**").permitAll()
+                .antMatchers("/api/post/{id}/comments").permitAll()
                 .anyRequest().authenticated()
                 ;
         http.addFilterBefore(getJsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
